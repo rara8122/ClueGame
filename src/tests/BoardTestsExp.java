1b3317a54ad0fb423f82c6b200aab1964df61917
MancMelP <melanie.mancera23@gmail.com>
2023-10-05 13:20:04 -0600

    small additions to boardtestexp class

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -1,10 +1,16 @@
 package tests;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import experiment.TestBoard;
+import experiment.TestBoardCell;
 
 public class BoardTestsExp {
 	TestBoard board; 
 	
-	//@BeforeEach //run before each test
+	@BeforeEach //run before each test
 	
 	public void setUp() {
 		board = new TestBoard();
@@ -13,5 +19,10 @@ public class BoardTestsExp {
 	/*
 	 * Test for adjacencies will start here. 
 	 */
-
+	@Test
+	public void testAdjacency() {
+		TestBoardCell cell = board.getCell(0,0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+	}
+	
 }
