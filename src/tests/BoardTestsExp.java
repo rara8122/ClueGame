ad4f5b32805537f8bc722a5a4430e4c72cc89d5d
MancMelP <melanie.mancera23@gmail.com>
2023-10-05 13:11:26 -0600

    additions to tests class

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -1,5 +1,17 @@
 package tests;
+import experiment.TestBoard;
 
 public class BoardTestsExp {
+	TestBoard board; 
+	
+	//@BeforeEach //run before each test
+	
+	public void setUp() {
+		board = new TestBoard();
+	}
+	
+	/*
+	 * Test for adjacencies will start here. 
+	 */
 
 }
