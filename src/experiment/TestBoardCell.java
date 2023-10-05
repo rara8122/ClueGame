41d50280247007e6dd2e1fd0b14801d9de40e787
MancMelP <melanie.mancera23@gmail.com>
2023-10-05 12:15:15 -0600

    added method comments to testboard class methods

--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -18,32 +18,32 @@ public class TestBoardCell {
     	this.adjacencyList = new HashSet<>();
     }
    
-    // Method to add a cell to the adjacency list 
+    //This method adds a cell to the adjacency list 
     public void addAdjacency(TestBoardCell cell) {
     	adjacencyList.add(cell);
     }
     
-    // Setter to get the adjacency list for the cell
+    // This is the method to get the adjacency list for the cell
     public Set<TestBoardCell> getAdjList() {
         return adjacencyList;
     }
 
-    // Setter to set if the cell is part of a room
+    // This method is used to set if the cell is part of a room
     public void setRoom(boolean isRoom) {
         this.isRoom = isRoom;
     }
     
-    // Getter to check if the cell is part of a room
+    // Method to check if the cell is part of a room
     public boolean isRoom() {
         return isRoom;
     }
 
-    // Setter to set if the cell is occupied by another player
+    // This is the method to set if the cell is occupied by another player
     public void setOccupied(boolean isOccupied) {
         this.isOccupied = isOccupied;
     }
 
-    // Getter to check if the cell is occupied by another player
+    // This method is to check if the cell is occupied by another player
     public boolean getOccupied() {
         return isOccupied;
     }
