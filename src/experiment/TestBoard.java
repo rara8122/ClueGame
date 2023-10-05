571f235f494524b4c9263a09284c70f57173b491
MancMelP <melanie.mancera23@gmail.com>
2023-10-05 12:24:55 -0600

    start of testboard class

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,5 +1,25 @@
 package experiment;
 
+import java.util.Set;
+
 public class TestBoard {
+	private TestBoardCell[][] grid;
+	private Set<TestBoardCell> targets;
+	
+	public TestBoard() {
+		
+	}
+	
+	public void calcTargets(TestBoardCell startCell, int pathlength) {
+		
+	}
+	
+	public TestBoardCell getCell(int row, int column) {
+		return null;
+	}
+
+	public Set<TestBoardCell> getTargets(){
+		return targets;
+	}
 
 }
