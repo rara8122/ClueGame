7ba713c440e85eba1d93cfe38fea8798c4bf705c
MancMelP <melanie.mancera23@gmail.com>
2023-10-31 14:39:04 -0600

    added additional classes Card, CardPlayer, Player, etc

---
+++ src/clueGame/Card.java
@@ -0,0 +1,21 @@
+package clueGame;
+
+public class Card {
+	
+	private String cardName;
+	
+	public boolean equals() {
+		return false;	
+	}
+
+	//getters
+	public String getCardName() {
+		return cardName;
+	}
+	
+	//setters
+	public void setCardName(String cardName) {
+		this.cardName = cardName;
+	}
+
+}
