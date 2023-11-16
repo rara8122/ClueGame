87ac6aea70e3e7a7686cc5837f0da9ef95c620ea
Rachel <rara8122@Blackie.local>
2023-11-15 18:55:33 -0700

    board click logic

--- src/clueGame/MisClick.java
+++ src/clueGame/MisClick.java
@@ -1,13 +1,13 @@
 package clueGame;
 
-public class PlayerNotFinished extends Exception{
+public class MisClick extends Exception{
 	//default constructor with a default message 
-	public PlayerNotFinished() {
+	public MisClick() {
 		super("Player is not finished with their turn");
 	}
 	
 	//constructor with a specific message
-	public PlayerNotFinished(String message) {
+	public MisClick(String message) {
 		super(message);
 	}
 }
